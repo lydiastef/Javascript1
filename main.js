40e0c2e7720f2a866cd992a4e4a432dffabb0e87
Lydia <lydiadoula@gmail.com>
2022-09-15 10:22:38 +0100

    added to JS

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
 //coments in js file
 console.log('Hello from main.js')
 
-// Variables
-let age = 25
-console.log(age)
+// Primitive Variables
 
-const salary = 8000
-console.log(salary)
+// Numbers
+// let can change
+let age = 25;
+console.log(age)
+let salary = 8000;
+//const doesn't change
+const monthsinaYear = 12;
+const boilingPointW = 100;
+
+// booleans - true or false
+let glassisFull = true;
+let maryisHome = false;
+
+// strings
+let msg = 'what should we do?';
+let name = 'Heidi';
+
+// null
+let selectedColor = null;
+
+// Reference Types
+// object
+let horse = {
+    name: 'Whitey',
+    age: 12
+};
+// Dot Notation
+horse.name = 'Blackey';
+
+// Bracket Notation
+horse['name'] = 'Stevie';
+
+// Arrays
+let selectedBreeds = ['pug', 'labrador', 'doberman'];
+console.log(selectedBreeds);
+
